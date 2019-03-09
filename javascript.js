84bf9d249d80fcf540ae3b1e78a77d53a07e10b1
PINPAL <josh.hayes614@gmail.com>
2019-03-09 00:29:17 +0000

    Edit Buttons

--- javascript.js
+++ javascript.js
@@ -14,6 +14,13 @@ var config = loadFile("config.txt").replace(/\r?\n|\r/g,"")
 var categories = config.split("#")
 for (i = 1; i < categories.length; i++) {
     section = categories[i].split(";");
+
+    var editButtonWrapper = document.createElement("div")
+    editButtonWrapper.className = "editButtonWrapper"
+    var editButton = document.createElement("button")
+    editButton.className = "editButton";
+    editButton.innerText = "Add New Link";
+    editButtonWrapper.appendChild(editButton);
     
     var sectionHeader = document.createElement("h1");
     sectionHeader.innerText = section[0].replace(/\[.\]/,"")
@@ -49,5 +56,22 @@ for (i = 1; i < categories.length; i++) {
     }
 
     bigBox.appendChild(a);
+    bigBox.appendChild(editButtonWrapper);
     document.getElementsByClassName("column")[columnForSection].appendChild(bigBox);
+}
+
+function editMode() {
+    if (document.getElementById("editModeButton").innerText == "Edit Page") {
+        var editButtons = document.getElementsByClassName("editButton")
+        for(i=0; i < editButtons.length; i++) {
+         editButtons[i].style.display = "inline";
+       }
+       document.getElementById("editModeButton").innerHTML = 'Save Changes'
+    } else {
+        document.getElementById("editModeButton").innerHTML = 'Edit Page'
+        var editButtons = document.getElementsByClassName("editButton")
+        for(i=0; i < editButtons.length; i++) {
+         editButtons[i].style.display = "none";
+       }
+    }
 }
